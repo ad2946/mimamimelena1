95825953aa912c617876aa4590a157e573c4d2bd
ad2946 <adrigarort@gmail.com>
2018-06-07 13:08:39 +0200

    Productos actualizado

--- www/pruebascript.js
+++ www/pruebascript.js
@@ -11,9 +11,22 @@ function cargarDatos () {
 		console.log(json);
 
 		json.forEach(function(obj) { 
-			opcion ="<div class='col-lg-3 col-sm-6 inner-top-sm animated fadeInUp'><div class='plan'><header><img src=\""+obj.imagen+"\"></header>"+"<ul class='features'><li>Nombre: "+obj.nombre+"</li><li>Precio: "+obj.precio+"€</li><li>Descripción: "+obj.Descripcion+"</li><li>Categoria: "+obj.tipo_producto+"</li></ul></div></div>"
+			opcion ="<div class='col-lg-3 col-sm-6 inner-top-sm animated fadeInUp'><div class='plan'><header><img src=\""+obj.imagen+"\"></header>"+"<ul class='features'><li>Nombre: "+obj.nombre+"</li><li>Precio: "+obj.precio+"€</li><li>Descripción:<br><button id='button' onclick='clickDescripcion()'></button><p id='descripcion'>"+obj.Descripcion+"</p></li><li>Categoria: "+obj.tipo_producto+"</li></ul></div></div>"
 			$("#productos").append(opcion);
 		});
 		
 	});
 }
+
+
+function clickDescripcion() {
+    var x = document.getElementById("descripcion");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
