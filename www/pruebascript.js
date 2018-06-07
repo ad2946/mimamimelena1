1f7a98a8a5e754d1b875855f90ad805c1cd70d69
ad2946 <adrigarort@gmail.com>
2018-06-07 12:05:01 +0200

    Pagina productos

---
+++ www/pruebascript.js
@@ -0,0 +1,19 @@
+function cargarDatos () {
+	console.log("Se inicia la carga de datos");
+	
+	//se solicitan las provincias...
+	$.get( "cargarDatosbasedatos.php", function( data ) {
+		console.log( "datos descargados: " + data );
+		
+		// pasar de String a un array json
+		var json = JSON.parse(data);
+		
+		console.log(json);
+
+		json.forEach(function(obj) { 
+			opcion ="<div class='col-lg-3 col-sm-6 inner-top-sm animated fadeInUp'><div class='plan'><header><img src=\""+obj.imagen+"\"></header>"+"<ul class='features'><li>Nombre: "+obj.nombre+"</li><li>Precio: "+obj.precio+"€</li><li>Descripción: "+obj.Descripcion+"</li><li>Categoria: "+obj.tipo_producto+"</li></ul></div></div>"
+			$("#productos").append(opcion);
+		});
+		
+	});
+}
