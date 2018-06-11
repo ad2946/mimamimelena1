917cc293b1aac05f934aa77c2cf34ece7dafec58
Salf1997 <sofia.lopez_97@hotmail.com>
2018-06-11 12:20:53 +0200

    Modificaciones 11 de junio v1

--- www/js/pruebascript.js
+++ www/js/pruebascript.js
@@ -12,15 +12,19 @@ function cargarDatos () {
 
 		json.forEach(function(obj) { 
 			opcion ="<div class='col-lg-3 col-sm-6 inner-top-sm animated fadeInUp'><div class='plan'>"+
-			"<header><img src=\""+obj.imagen+"\"></header>"+"<ul class='features'><li>Nombre: "+obj.nombre+"</li><li>Precio: "+
-			obj.precio+"€</li><li>Descripción:<br><button type='button' class='btn btn-info' data-toggle='collapse' data-target='#descripcion"+obj.id_productoM+"'>Mostrar"+
+			"<header><img src=\""+obj.imagen+"\"></header>"+"<ul class='features'><li><strong>Nombre:</strong> "+obj.nombre+"</li><li>Precio: "+
+			obj.precio+"€</li><li><button type='button' class='btn btn-info' data-toggle='collapse' data-target='#descripcion"+obj.id_productoM+"'>Descripci&oacuten"+
 			"</button><div id='descripcion"+obj.id_productoM+"' class='collapse'>"+obj.Descripcion+"</div>"+
-			"</li><li>Categoria: "+obj.tipo_producto+"</li></ul></div></div>"
+			"</li></ul></div></div>"
 			$("#productos").append(opcion);
 		});
 		
 	});
 }
+
+/*
+<li>Categoria: "+obj.tipo_producto+"</li>
+*/
 
 
 
